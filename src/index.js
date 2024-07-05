fbb63e91ff25aeb89a3c6b87d08f0c60b30e8a29
Deepak Chauhan <am.deepakkumarchauhan@gmail.com>
2024-07-05 08:53:37 +0530

    Setup of node-cron for mail sending automation

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
-
-const { sendBasicEmail } = require("./services/email-service");
+const cron = require("node-cron");
+//const { sendBasicEmail } = require("./services/email-service");
 //const db = require("./models/index");
 
 const setupAndStartServer = () => {
@@ -13,15 +13,15 @@ const setupAndStartServer = () => {
 
   app.listen(PORT, () => {
     console.log("Server started sucessfully...");
-    sendBasicEmail(
-      "support@admin.com",
-      "am.deepakkumarchauhan@gmail.com",
-      "This is a testing email from our team.",
-      "Hey, I hope you like the service of reminders. lots of love."
-    )
-    if (process.env.DB_SYNC) {
-      db.sequelize.sync({ alter: true });
-    }
+    // sendBasicEmail(
+    //   "support@admin.com",
+    //   "am.deepakkumarchauhan@gmail.com",
+    //   "This is a testing email from our team.",
+    //   "Hey, I hope you like the service of reminders. lots of love."
+    // );
+    cron.schedule("*/2 * * * * ", () => {
+      console.log("Running a task every 2 minutes.");
+    });
   });
 };
 
