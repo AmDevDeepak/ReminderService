b22d755c939af806f3233f319c4f97f23e3740dd
Deepak Chauhan <am.deepakkumarchauhan@gmail.com>
2024-07-05 07:22:51 +0530

    Initial Setup commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const { PORT } = require("./config/serverConfig");
+
+//const db = require("./models/index");
+
+const setupAndStartServer = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, () => {
+    console.log("Server started sucessfully...");
+
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
+  });
+};
+
+setupAndStartServer();
