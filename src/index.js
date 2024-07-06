0108a003a57fdce3521a1dc100a98fc51516c72c
Deepak Chauhan <am.deepakkumarchauhan@gmail.com>
2024-07-06 16:01:50 +0530

    Added RabitMQ config

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
+const { createChannel } = require("./utils/messageQueue");
+
 const ticketController = require("./controllers/ticket-controller");
 const jobs = require("./utils/job");
-const setupAndStartServer = () => {
+
+const setupAndStartServer = async () => {
+  const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  //const channel = await createChannel();
 
   app.post("/api/v1/tickets", ticketController.create);
   app.listen(PORT, () => {
     console.log("Server started sucessfully...");
-    jobs();
-    // sendBasicEmail(
-    //   "support@admin.com",
-    //   "am.deepakkumarchauhan@gmail.com",
-    //   "This is a testing email from our team.",
-    //   "Hey, I hope you like the service of reminders. lots of love."
-    // );
+    //jobs();
   });
 };
 
